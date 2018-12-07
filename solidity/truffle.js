c30ce53546123174a3e47c4687cbdac75aa6f396
Alex Coventry <coventry@gmail.com>
2018-12-07 16:10:08 -0500

    Set the compiler version, use docker-based compiler

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -3,6 +3,12 @@ require('babel-polyfill');
 
 module.exports = {
   network: "test",
+  compilers: {
+    solc: {
+      version: "0.4.24",
+      docker: true,
+    }
+  },
   networks: {
     development: {
       host: "127.0.0.1",

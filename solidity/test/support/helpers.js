2132548eae3cc677320a483514cda9d862a75c68
Alex Coventry <coventry@gmail.com>
2018-12-13 18:41:34 -0500

    Describe role of helpers.js/PRIVATE_KEY

--- solidity/test/support/helpers.js
+++ solidity/test/support/helpers.js
@@ -13,6 +13,7 @@ process.env.SOLIDITY_INCLUDE = [
   'node_modules/@ensdomains/ens/contracts/',
 ].map(p => join(contractPathHead, p)).join(':')
 
+// Key for defaultAccount, defined below.
 const PRIVATE_KEY = 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'
 
 const Wallet = require('../../app/wallet.js')

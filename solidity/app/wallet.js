75f7da4ab4e8b3e31aa89380fd9c76c632821981
Alex Coventry <coventry@gmail.com>
2018-12-16 15:19:13 -0500

    Revert wallet.js instrumentation, now that nonce issue is fixed

--- solidity/app/wallet.js
+++ solidity/app/wallet.js
@@ -1,39 +1,26 @@
-var AsyncLock = require('async-lock');  // XXX: Convert to `import` syntax
-
 const Tx = require('ethereumjs-tx')
 const EthWallet = require('ethereumjs-wallet')
-
-const nonceLock = new AsyncLock()
 
 module.exports = function Wallet (key, utils) {
   const privateKey = Buffer.from(key, 'hex')
   const wallet = EthWallet.fromPrivateKey(privateKey)
   const address = `0x${wallet.getAddress().toString('hex')}`
   const eth = utils.eth
-  const nextNonce = async () => await eth.getTransactionCount(address, 'pending')
+  const nextNonce = async () => eth.getTransactionCount(address, 'pending')
 
   return {
     address: address,
     nextNonce: nextNonce,
-    send: async (params) => await nonceLock.acquire(
-      `Transaction lock for ${address}`,
-      async () => {
-        const lockID = Math.random()
-        console.log('entering lock for', address, 'lock ID', lockID, Date.now())
-        const defaults = {
-          nonce: await nextNonce(),
-          chainId: 0
-        }
-        console.log('default nonce', defaults.nonce, 'lock ID', lockID, Date.now())
-        let tx = new Tx(Object.assign(defaults, params))
-        console.log('tx nonce', tx.nonce, 'lock ID', lockID, Date.now())
-        tx.sign(privateKey)
-        let txHex = tx.serialize().toString('hex')
-        console.log('sending tx', 'lock ID', lockID, Date.now())
-        result = await eth.sendRawTransaction(txHex)
-        console.log('done sending tx for lock id ${lockID}', Date.now())
-        return result
-      }),
+    send: async (params) => {
+      const defaults = {
+        nonce: await nextNonce(),
+        chainId: 0
+      }
+      let tx = new Tx(Object.assign(defaults, params))
+      tx.sign(privateKey)
+      let txHex = tx.serialize().toString('hex')
+      return eth.sendRawTransaction(txHex)
+    },
     call: async (params) => {
       return eth.call(params)
     }
